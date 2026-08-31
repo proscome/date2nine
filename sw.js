e34ccb2f789596b47d59fc7c8cd940eb16dddb60
proscome <masakatsu.seko@gmail.com>
2026-09-01 08:47:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-javascript
-const CACHE = 'date2nine-v3';
-
+const CACHE = 'date2nine-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -10,75 +8,23 @@ const ASSETS = [
   './icons/icon-512.png'
 ];
 
-
-/* ==========================================
-   インストール
-   ========================================== */
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-
-/* ==========================================
-   有効化
-   古いキャッシュを削除
-   ========================================== */
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(k => k !== CACHE)
-          .map(k => caches.delete(k))
-      )
-    ).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
   );
 });
 
-
-/* ==========================================
-   ファイル取得
-   基本はネットから最新版を取得
-   オフラインの場合はキャッシュを使用
-   ========================================== */
 self.addEventListener('fetch', (e) => {
-
   const req = e.request;
-
-  /* GET以外はそのまま */
-  if (req.method !== 'GET') {
-    return;
-  }
-
   e.respondWith(
-
-    fetch(req)
-      .then((response) => {
-
-        /* 正常なレスポンスならキャッシュも更新 */
-        if (response && response.ok) {
-
-          const copy = response.clone();
-
-          caches.open(CACHE).then((cache) => {
-            cache.put(req, copy);
-          });
-
-        }
-
-        return response;
-      })
-
-      .catch(() => {
-
-        /* オフラインならキャッシュを使用 */
-        return caches.match(req);
-
-      })
-
+    caches.match(req).then(res => res || fetch(req))
   );
 });
